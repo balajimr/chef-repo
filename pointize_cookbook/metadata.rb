daa92660cea534148efa04c44e34ffaadfae48e2
Balaji Ravi <balaji@Balajis-MacBook-Air.local>
2014-09-25 13:18:43 +0530

    initial commit

---
+++ pointize_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'pointize_cookbook'
+maintainer       'Bizsciences'
+maintainer_email 'balaji@holidayjuggle.net'
+license          'Apache 2.0'
+description      'Installs/Configures pointize_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
